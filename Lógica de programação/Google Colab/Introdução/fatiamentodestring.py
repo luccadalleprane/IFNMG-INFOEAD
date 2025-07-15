5e40e2fb9cffc76dc0167e1991561e4ff1281b4b
LUCCA NASCIMENTO DALLEPRANE <119019100+luccadalleprane@users.noreply.github.com>
2025-07-15 16:07:46 -0300

    Add files via upload

---
+++ Lógica de programação/Google Colab/Introdução/fatiamentodestring.py
@@ -0,0 +1,14 @@
+# É uma ferramenta usada para extrair apenas uma parte dos
+# elementos de uma string.
+# Nome_String[Limite_Inferior: Limite_Superior]
+# ou
+# Nome_String[Limite_Inferior: Limite_Superior: passo]
+# ● Retorna uma string com os elementos das posições do limite
+# inferior até o limite superior - 1.
+
+s = 'Python'
+s[1:4]  # Retorna 'yth'
+
+s[2:]  # Retorna 'thon'
+
+s[:4]  # Retorna 'Pyth'
