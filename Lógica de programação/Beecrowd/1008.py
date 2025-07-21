d8a8e44ba2e4e87186297ea879b7841d555725e9
LUCCA NASCIMENTO DALLEPRANE <119019100+luccadalleprane@users.noreply.github.com>
2025-07-21 12:40:48 -0300

    Add files via upload

---
+++ Lógica de programação/Beecrowd/1008.py
@@ -0,0 +1,10 @@
+#1008
+#Entrada de dados
+A = int(input())
+worked = int(input())
+horas = float(input())
+#Cálculo do salário
+salary = worked * horas
+# Exibição do resultado formatado
+print(f"NUMBER = {A}")
+print(f"SALARY = U$ {salary:.2f}")
