f43a3089f0d21b3cee30464a8dddf1a08f5b951a
LUCCA NASCIMENTO DALLEPRANE <119019100+luccadalleprane@users.noreply.github.com>
2025-07-21 12:40:22 -0300

    Add files via upload

---
+++ Lógica de programação/Beecrowd/1006.py
@@ -0,0 +1,9 @@
+# 1006
+# Entrada de dados
+A = float(input())
+B = float(input())
+C = float(input())
+# Cálculo da média ponderada
+media = ((2*A) + (3*B) + (5*C)) / 10
+# Exibição do resultado formatado
+print(f"MEDIA = {media:.1f}")
