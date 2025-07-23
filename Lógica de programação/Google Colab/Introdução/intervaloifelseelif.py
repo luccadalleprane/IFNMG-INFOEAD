718f246aa900e4f3055971c6a6d49975fe477d5f
LUCCA NASCIMENTO DALLEPRANE <119019100+luccadalleprane@users.noreply.github.com>
2025-07-22 23:47:36 -0300

    Add files via upload

---
+++ Lógica de programação/Google Colab/Introdução/intervaloifelseelif.py
@@ -0,0 +1,10 @@
+a = int(input("Digite o valor de a: "))
+if -10 < a and a < 10:
+	print('(-10, 10) é o intervalo de a.')
+else:
+	print('(-10, 10) não é o intervalo de a.')
+a = int(input("Digite o valor de a novamente: "))
+if -10 < a < 10:
+	print('(-10, 10) é o intervalo de a.')
+else:
+	print('(-10, 10) não é o intervalo de a.')
